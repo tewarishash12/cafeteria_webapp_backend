c180e136b2660103fc4f662a5439a4632f718a63
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-24 20:02:29 +0530

    Added the dish routes and it's corresponding controllers

---
+++ main_server/main_controllers/dishControllers.js
@@ -0,0 +1,49 @@
+const Dish = require("../../models/dish");
+
+exports.allDishDetails = async (req, res) => {
+    try {
+        const dishes = await Dish.find().select('-__v -_id');
+        if (!dishes)
+            return res.status(404).json({ message: "Something unexpected is requested" })
+        res.status(201).json(dishes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+exports.addDish = async (req, res) => {
+    try {
+        const dish = new Dish(req.body);
+        const result = await dish.save();
+        res.status(201).json({ message: "New user created successfully" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+exports.deleteDishById = async (req, res) => {
+    try {
+        const dishId = await Dish.findById(req.params.id);
+        if (!dishId)
+            return res.status(404).json({ message: "Requested user doesn't exist" });
+        const dish = await Dish.findByIdAndDelete(req.params.id);
+        res.status(201).json(dish);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+exports.updateDishById = async (req, res) => {
+    try {
+        const { dish_name, description, image, price, availability, counter_id } = req.body;
+        const dishId = await Dish.findById(req.params.id);
+        
+        if (!dishId)
+            return res.status(404).json({ message: "Requested user doesn't exist" });
+
+        const user = await Dish.findByIdAndUpdate({ _id: req.params.id }, { dish_name: dish_name, description: description, image: image, price: price, availability: availability, counter_id: counter_id }, { new: true });
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
--- main_server/main_routes/dishRoutes.js
+++ main_server/main_routes/dishRoutes.js
@@ -1,3 +1,13 @@
 const router = require("express").Router();
 
+const { allDishDetails, addDish, deleteDishById, updateDishById } = require("../main_controllers/dishControllers");
+
+router.get("/alldishes", allDishDetails);
+
+router.post("/", addDish);
+
+router.delete("/id/:id", deleteDishById);
+
+router.put("/id/:id", updateDishById);
+
 module.exports = router;
