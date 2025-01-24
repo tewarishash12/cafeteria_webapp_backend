25c05106b5e929d4f7e7ea3042b6dbd6f7385f18
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-24 20:01:41 +0530

    Added reference and the password to the database

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const CartItem = new mongoose.Schema({
-    item: [{type: mongoose.Schema.Types.ObjectId}],
+    item: [{type: mongoose.Schema.Types.ObjectId, ref:"Dish"}],
     quantity: {type:Number, min:1 }
 })
 
 const UserSchema = new mongoose.Schema({
-    username: {type:String, required:true},
-    email: {type:String, required:true},
+    username: {type:String, required:true, unique:true},
+    email: {type:String, required:true, unique:true},
     phoneNo: {type:String, required:true},
     role: {type:String, enum:["customer", "merchant", "admin"],required:true, default:"customer"},
     cart: {type:[CartItem]},
-    // password: {type:String, required:true},
+    password: {type:String, required:true},
 })
 
 module.exports = mongoose.model("User", UserSchema)
