2f06eba3629afeafaeb6ba45a6bb749ad00e653a
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-24 20:00:35 +0530

    removed the counter_id and added the shop_name field

--- models/counter.js
+++ models/counter.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const CounterSchema = new mongoose.Schema({
-    merchant_id: {type:[mongoose.Schema.Types.ObjectId]},
-    dish_id: {type:[mongoose.Schema.Types.ObjectId], required:true}
+    merchant_id: {type:[mongoose.Schema.Types.ObjectId], ref:"User", required:true},
+    shop_name: {type:String, required:true}
 })
 
 module.exports = mongoose.model("Counter", CounterSchema)
