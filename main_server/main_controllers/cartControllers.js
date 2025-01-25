aa5d3fe417bc8b3baf3b21869f3f93a2f87a1141
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-26 01:16:17 +0530

    created the files to setup the cart routes and it's operation

---
+++ main_server/main_controllers/cartControllers.js
@@ -0,0 +1,10 @@
+const Cart = require("User");
+
+exports.addToCart = async(req,res) =>{
+    try {
+        // const dish_id = req.params.id;
+        // const user_id = 
+    } catch(err) {
+        res.status(500).json({message:err.message})
+    }
+}
