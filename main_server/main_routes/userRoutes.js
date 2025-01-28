83fef5755dc6c56fbe441616db58b90665915375
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-28 20:44:21 +0530

    meroute for merchant call

--- main_server/main_routes/userRoutes.js
+++ main_server/main_routes/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
-const { allUserDetails, userDetailById, createUser, deleteUserById, updateUser, me  } = require("../main_controllers/userControllers");
+const { allUserDetails, userDetailById, createUser, deleteUserById, updateUser, me, getMerchants  } = require("../main_controllers/userControllers");
+
 const { authLogin } = require("../middlewares/authMiddleware");
 
 router.get('/', allUserDetails);
@@ -12,6 +13,8 @@ router.get('/id/:id', userDetailById);
 router.delete("/id/:id", deleteUserById);
 
 router.put("/id/:id", updateUser);
+
+router.get("/merchants", getMerchants);
 
 router.get("/me", authLogin, me)
 
