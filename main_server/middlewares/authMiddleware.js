1f57fd85627ee05a50759cb85cb04ed017700acd
tewarishash12 <tewarishashwat2002@gmail.com>
2025-01-29 15:49:10 +0530

    Created middleware for role based authentication

--- main_server/middlewares/authMiddleware.js
+++ main_server/middlewares/authMiddleware.js
@@ -1,19 +1,31 @@
 const jwt = require("jsonwebtoken");
 
-function authLogin(req,res,next){
+function authLogin(req, res, next) {
     const authHeader = req.headers['authorization'];
 
     const accesstoken = authHeader && authHeader.split(" ")[1];
 
-    if(!accesstoken)
-        return res.status(400).json({message:"Access token is tampered"});
+    if (!accesstoken)
+        return res.status(400).json({ message: "Access token is tampered" });
 
-    jwt.verify(accesstoken, process.env.ACCESS_TOKEN_SECRET, (err,data)=>{
-        if(err) return res.status(403).json({message: "Unauthorized to access"});
+    jwt.verify(accesstoken, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+        if (err) return res.status(403).json({ message: "Unauthorized to access" });
 
         req.user = data.user;
         next();
     })
 }
 
-module.exports = {authLogin}
+function adminRoleValidation() {
+    if (req.user.role = "admin")
+        next();
+    return res.status(400).json({ message: "You are unauthorised to access this page" })
+}
+
+function merchantRoleValidation() {
+    if (req.user.role = "merchant")
+        next();
+    return res.status(400).json({ message: "You are unauthorised to access this page" })
+}
+
+module.exports = { authLogin,adminRoleValidation,merchantRoleValidation  }
