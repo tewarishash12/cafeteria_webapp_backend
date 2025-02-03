e8a701870a977d59dd5a3180b130d781bb6c967a
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-03 12:45:24 +0530

    `Added fields to CounterSchema in counter.js`

--- main_server/main_model/counter.js
+++ main_server/main_model/counter.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const CounterSchema = new mongoose.Schema({
     merchant_id: [{type:mongoose.Schema.Types.ObjectId, ref:"User", required:true}],
+    description: {type:String},
+    image: {type: String},
+    hours: {type: String},
+    isActive: {type: Boolean},
     shop_name: {type:String, required:true}
 })
 
