6d000a47708a5f737fe9ecf172f57039b3f25879
tewarishash12 <tewarishashwat2002@gmail.com>
2025-02-05 16:08:10 +0530

    Made some minor fixes int he middleware

--- main_server/middlewares/authMiddleware.js
+++ main_server/middlewares/authMiddleware.js
@@ -2,29 +2,29 @@ const jwt = require("jsonwebtoken");
 
 function authLogin(req, res, next) {
     const authHeader = req.headers['authorization'];
-
+    
     const accesstoken = authHeader && authHeader.split(" ")[1];
-
+    
     if (!accesstoken)
         return res.status(400).json({ message: "Access token is tampered" });
-
+    
     jwt.verify(accesstoken, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
         if (err) return res.status(403).json({ message: "Unauthorized to access" });
 
         req.user = data.user;
-        next();
+        next(); 
     })
 }
 
-function adminRoleValidation() {
+function adminRoleValidation(req,res,next) {
     if (req.user.role !== "admin")
-        return res.status(400).json({ message: "You are unauthorised to interact with this information" })
+        return res.status(403).json({ message: "You are unauthorised to interact with this information" })
     next();
 }
 
-function merchantRoleValidation() {
+function merchantRoleValidation(req,res,next) {
     if (req.user.role !== "merchant")
-        return res.status(400).json({ message: "You are unauthorised to interact with this information" })
+        return res.status(403).json({ message: "You are unauthorised to interact with this information" })
     next();
 }
 
